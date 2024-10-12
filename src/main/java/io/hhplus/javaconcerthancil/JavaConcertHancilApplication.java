f84683a86bf56fca267056c7998427bb76cd8b0c
THIRSTY <dev.thirsty92@gmail.com>
2024-10-12 17:20:23 +0900

    프로젝트 초기화

---
+++ src/main/java/io/hhplus/javaconcerthancil/JavaConcertHancilApplication.java
@@ -0,0 +1,13 @@
+package io.hhplus.javaconcerthancil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConcertHancilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaConcertHancilApplication.class, args);
+	}
+
+}
