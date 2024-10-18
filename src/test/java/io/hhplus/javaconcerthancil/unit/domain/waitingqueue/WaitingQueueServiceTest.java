c75259c9a9b59e5f4b679ad1196f26227a4615db
THIRSTY <beta1992@hanmail.net>
2024-10-18 11:22:21 +0900

    fix 대기열서비스 단위테스트

--- src/test/java/io/hhplus/javaconcerthancil/unit/domain/waitingqueue/WaitingQueueServiceTest.java
+++ src/test/java/io/hhplus/javaconcerthancil/unit/domain/waitingqueue/WaitingQueueServiceTest.java
@@ -1,68 +1,131 @@
 package io.hhplus.javaconcerthancil.unit.domain.waitingqueue;
 
 import io.hhplus.javaconcerthancil.domain.waitingqueue.*;
+import io.hhplus.javaconcerthancil.web.waitingqueue.response.IssueTokenResponse;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
+@SpringBootTest
 public class WaitingQueueServiceTest {
 
-    @InjectMocks
-    private WaitingQueueService waitingQueueService;
 
-    @Mock
+    @Autowired
     private WaitingQueueTokenProvider tokenProvider;
 
-    @Mock
+    @Autowired
     private WaitingQueueRepository queueRepository;
+
+    @Autowired
+    private WaitingQueueService waitingQueueService;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this);
+        waitingQueueService = new WaitingQueueService(tokenProvider, queueRepository);
+        queueRepository.save(new WaitingQueue(1L, "token1"));
     }
 
     @Test
-    void 토큰을_발급받는다() {
+    @DisplayName("토큰을 발급받는다.")
+    @Transactional
+    void issueToken1() {
+
+        // Given
+        Long userId = 99L;
+
+        // When
+        String issueToken = waitingQueueService.issueToken(userId);
+
+        // Then
+        assertNotNull(issueToken);
+    }
+
+    @Test
+    @DisplayName("queue에 해당 유저의 토큰이 존재할 경우 기존 대기열 토큰을 반환한다.")
+    @Transactional
+    void issueToken2() {
+        //given
+        Long userId = 1L;
+
+        //when
+        Optional<WaitingQueue> tokenByUserId = waitingQueueService.getTokenByUserId(userId);
+
+        //then
+        assertTrue(tokenByUserId.isPresent());
+
+        assertThat(tokenByUserId.get().getToken()).isEqualTo("token1");
+    }
+
+
+    @Test
+    @DisplayName("토큰으로_대기열엔티티_조회")
+    @Transactional
+    void findByTokenTest() {
+        //given
+        String givenToken = "token1";
+
+        //when
+        WaitingQueue byToken = waitingQueueService.findByToken(givenToken);
+
+        //then
+        assertNotNull(byToken);
+        assertThat(byToken.getToken()).isEqualTo("token1");
+        assertThat(byToken.getStatus()).isEqualTo(QueueStatus.STANDBY);
+    }
+
+    @Test
+    @DisplayName("대기번호조회")
+    @Transactional
+    void getWaitingNumberTest() {
+        //given
+        Long userId = 1L;
+
+        //when
+        long waitingNumber = waitingQueueService.getWaitingNumber(userId);
+
+        //then
+        assertNotNull(waitingNumber);
+        assertThat(waitingNumber).isEqualTo(0);
+
+    }
+
+    @Test
+    @DisplayName("대기열 활성화 업데이트")
+    @Transactional
+    void updateTokenActivateTest() {
 
         // Given
         Long userId = 1L;
-        String expectedToken = UUID.randomUUID().toString();
-        given(tokenProvider.createQueueToken()).willReturn(expectedToken);
+        Optional<WaitingQueue> optionalWaitingQueue = waitingQueueService.getTokenByUserId(userId);
+        assertTrue(optionalWaitingQueue.isPresent());
+
+        WaitingQueue queueItem = optionalWaitingQueue.get();
 
         // When
-        String actualToken = waitingQueueService.issueToken(userId);
+        WaitingQueue waitingQueue = waitingQueueService.updateTokenActivate(queueItem);
 
-        // Then
-        assertEquals(expectedToken, actualToken);
-        verify(queueRepository, times(1)).save(any(WaitingQueue.class));
+        //then
+        assertThat(waitingQueue.getToken()).isEqualTo("token1");
+        assertThat(waitingQueue.getStatus()).isEqualTo(QueueStatus.PROGRESS);
+
     }
 
-    @Test
-    void 대기열에서_대기중인_모든_항목_조회_테스트() {
-
-        // Given
-        WaitingQueue queueItem1 = new WaitingQueue(1L, "token1");
-        WaitingQueue queueItem2 = new WaitingQueue(2L, "token2");
-        given(queueRepository.findAllByStatusIn(any())).willReturn(List.of(queueItem1, queueItem2));
-
-        // When
-        List<WaitingQueue> result = waitingQueueService.getWaitingTotalInQueue();
-
-        // Then
-        assertEquals(2, result.size());
-        verify(queueRepository, times(1)).findAllByStatusIn(any());
-    }
 
 
 }
