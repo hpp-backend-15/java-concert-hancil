5052d0d806c80f8f1e8b9a8343b4a7949b74d7b6
THIRSTY <beta1992@hanmail.net>
2024-10-23 16:17:08 +0900

    feat user - 포인트 충전/사용 단위테스트

--- src/main/java/io/hhplus/javaconcerthancil/domain/user/User.java
+++ src/main/java/io/hhplus/javaconcerthancil/domain/user/User.java
@@ -1,7 +1,10 @@
 package io.hhplus.javaconcerthancil.domain.user;
 
+import io.hhplus.javaconcerthancil.interfaces.api.common.ApiException;
+import io.hhplus.javaconcerthancil.interfaces.api.common.ErrorCode;
 import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.logging.LogLevel;
 
 import java.util.List;
 
@@ -22,5 +25,25 @@ public class User {
     public User(String name) {
         this.name = name;
         this.balance = 0;
+    }
+
+    public void addAmount(int amount) {
+        if(amount <= 0){
+            throw new ApiException(ErrorCode.E006, LogLevel.INFO, "amount must be positive.");
+        }
+
+
+        this.balance += amount;
+    }
+
+    public void subtractAmount(int amount) {
+        if (this.balance < amount) {
+            throw new ApiException(ErrorCode.E005, LogLevel.INFO, "userAmount = " + amount + "this.amount = " + this.balance);
+        }
+        this.balance -= amount;
+    }
+
+    public long getBalance() {
+        return balance;
     }
 }
