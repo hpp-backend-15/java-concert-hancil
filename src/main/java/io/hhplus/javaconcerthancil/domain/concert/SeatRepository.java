3269eb2c8dd15b8f7dadf17ab86d08730fd52fe6
THIRSTY <beta1992@hanmail.net>
2024-10-23 12:16:21 +0900

    fix 예약 동시성 테시트 + 비관적 락 적용

--- src/main/java/io/hhplus/javaconcerthancil/domain/concert/SeatRepository.java
+++ src/main/java/io/hhplus/javaconcerthancil/domain/concert/SeatRepository.java
@@ -1,6 +1,15 @@
 package io.hhplus.javaconcerthancil.domain.concert;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT s FROM Seat s WHERE s.id IN :seatIds")
+    List<Seat> findAllByIdForUpdate(List<Long> seatIds);
 }
--- src/main/java/io/hhplus/javaconcerthancil/domain/reservation/ReservationService.java
+++ src/main/java/io/hhplus/javaconcerthancil/domain/reservation/ReservationService.java
@@ -5,7 +5,11 @@ import io.hhplus.javaconcerthancil.domain.concert.SeatRepository;
 import io.hhplus.javaconcerthancil.domain.concert.SeatStatus;
 import io.hhplus.javaconcerthancil.domain.user.User;
 import io.hhplus.javaconcerthancil.domain.user.UserRepository;
+import io.hhplus.javaconcerthancil.interfaces.api.common.ApiException;
+import io.hhplus.javaconcerthancil.interfaces.api.common.ErrorCode;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.logging.LogLevel;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,37 +24,46 @@ public class ReservationService {
     private final UserRepository userRepository;
     private final SeatRepository seatRepository;
 
+    @Transactional
     public Reservation reserveConcert(Long userId, Long concertId, Long scheduleId, List<Long> seatIds) {
 
         // 1. 사용자 확인
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new RuntimeException("User not found"));
+                .orElseThrow(() -> new ApiException(ErrorCode.E404, LogLevel.INFO, "User not found"));
 
-        Reservation reservation = new Reservation(user);
-        Reservation savedResrvation = reservationRepository.save(reservation);// 예약 먼저 저장
+        // 2. 좌석 유효성 검사
+//        List<Seat> seats = seatRepository.findAllById(seatIds);
+        List<Seat> seats = seatRepository.findAllByIdForUpdate(seatIds);
 
-        for (Long seatId : seatIds) {
-            Seat seat = seatRepository.findById(seatId)
-                    .orElseThrow(() -> new RuntimeException("Seat not found"));
-
-            if (!seat.getStatus().equals(SeatStatus.AVAILABLE)) {
-                throw new RuntimeException("Seat not available");
+        // 좌석이 없거나, 상태가 AVAILABLE이 아닌 좌석이 있는지 확인
+        for (Seat seat : seats) {
+            if (seat == null) {
+                throw new ApiException(ErrorCode.E404, LogLevel.INFO, "Seat not found");
             }
+            if (!seat.getStatus().equals(SeatStatus.AVAILABLE)) {
+                throw new ApiException(ErrorCode.E002, LogLevel.INFO, "Seat not available");
+            }
+        }
 
+        // 3. 예약 저장
+        Reservation reservation = new Reservation(user);
+        Reservation savedReservation = reservationRepository.save(reservation); // 예약 먼저 저장
+
+        for (Seat seat : seats) {
             // 좌석 상태 변경
             seat.setStatus(SeatStatus.RESERVED);
             seatRepository.save(seat);
 
             // 예약 항목 저장
             ReservationItem reservationItem = new ReservationItem();
-            reservationItem.setReservation(savedResrvation);
+            reservationItem.setReservation(savedReservation);
             reservationItem.setSeat(seat);
             reservationItem.setSeatPrice(seat.getSeatPrice());
 
             ReservationItem saveReservationItem = reservationItemRepository.save(reservationItem);
-            savedResrvation.addItem(saveReservationItem);
+            savedReservation.addItem(saveReservationItem);
         }
 
-        return savedResrvation;
+        return savedReservation;
     }
 }
