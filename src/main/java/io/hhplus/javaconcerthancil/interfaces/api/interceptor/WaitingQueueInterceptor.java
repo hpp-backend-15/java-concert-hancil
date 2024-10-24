e26dad891b855d4081b578859540c3220a2012cb
THIRSTY <beta1992@hanmail.net>
2024-10-24 12:25:10 +0900

    feat/step09 interceptor를 활용하여 기능 관점 분리

--- src/main/java/io/hhplus/javaconcerthancil/interfaces/api/interceptor/WaitingQueueInterceptor.java
+++ src/main/java/io/hhplus/javaconcerthancil/interfaces/api/interceptor/WaitingQueueInterceptor.java
@@ -22,6 +22,11 @@ public class WaitingQueueInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
+        // interceptor를 활용하여
+        // 지정된 요청에 따라 토큰의 유효성을 검증한다.
+        // 기존엔 모든 요청에 따라 Facade 혹은 Service 레벨에서 토큰을 매번 검증하였음
+
+
         //1. 토큰 만료 검증
         String token = request.getHeader("QUEUE_TOKEN");
         boolean isActive = queueService.isActiveToken(token);
