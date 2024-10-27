7c8122be05cfe84ee1b3be6a166b10bc0ca874a8
THIRSTY <dev.thirsty92@gmail.com>
2024-10-27 21:17:06 +0900

    fix: ConcertSchedule의 유효성 검사를 ConcertSchedule내부에서 해결할 수 있도록 개선

--- src/main/java/io/hhplus/javaconcerthancil/domain/concert/ConcertService.java
+++ src/main/java/io/hhplus/javaconcerthancil/domain/concert/ConcertService.java
@@ -18,13 +18,11 @@ public class ConcertService {
 
     public List<Seat> getConcertSeats(Long concertId, Long scheduleId) {
 
-//        // 1. Concert 엔티티 조회
-//        Concert concert = concertRepository.findById(concertId)
-//                .orElseThrow(() -> new IllegalArgumentException("Concert not found"));
-
-        // 2. ConcertSchedule 엔티티 조회
+        // 1. ConcertSchedule 엔티티 조회
         ConcertSchedule concertSchedule = concertScheduleRepository.findById(scheduleId)
                 .orElseThrow(() -> new IllegalArgumentException("ConcertSchedule not found"));
+
+        // 2. 콘서트 예약가능여부 및 concertId 유효성 검사
         concertSchedule.isValid(concertId);
 
         // 3. 좌석 목록 조회
