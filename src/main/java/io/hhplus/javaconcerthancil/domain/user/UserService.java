c38ac08ea924d05bb93b50b25c2a5fc4438c7444
THIRSTY <dev.thirsty92@gmail.com>
2024-10-30 00:46:55 +0900

    feat: 낙관적/비관적/분산락 - 포인트 충전 테스트

--- src/main/java/io/hhplus/javaconcerthancil/domain/user/UserService.java
+++ src/main/java/io/hhplus/javaconcerthancil/domain/user/UserService.java
@@ -1,5 +1,6 @@
 package io.hhplus.javaconcerthancil.domain.user;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,14 +11,29 @@ import java.util.Optional;
 public class UserService {
 
     private final UserRepository userRepository;
-    private final BalanceHistoryRepository balanceHistoryRepository;
+    private final UserWithVersionRepository userWithVersionRepository;
 
     public Optional<User> findById(long userId) {
         return userRepository.findById(userId);
     }
 
+    public Optional<UserWithVersion> findByIdWithVersion(long userId) {
+        return userWithVersionRepository.findById(userId);
+    }
+
+
+    @Transactional
+    public Optional<User> findByIdWithLock(long userId) {
+        return userRepository.findByIdWithLock(userId);
+    }
+
+
     public User updateBalance(User user) {
         return userRepository.save(user);
+    }
+
+    public UserWithVersion updateBalanceWithVersion(UserWithVersion user) {
+        return userWithVersionRepository.save(user);
     }
 
 //    public BalanceHistory saveHistory(BalanceHistory balanceHistory) {
