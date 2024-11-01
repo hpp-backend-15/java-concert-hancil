f2361157e10200c75c0d7d0e2be7be02952e17dc
THIRSTY <beta1992@hanmail.net>
2024-11-01 18:28:07 +0900

    fix 예약 서비스 동시성 제어 기능 수정

--- src/main/java/io/hhplus/javaconcerthancil/domain/concert/ConcertScheduleRepository.java
+++ src/main/java/io/hhplus/javaconcerthancil/domain/concert/ConcertScheduleRepository.java
@@ -1,6 +1,15 @@
 package io.hhplus.javaconcerthancil.domain.concert;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.Optional;
 
 public interface ConcertScheduleRepository extends JpaRepository<ConcertSchedule, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT e FROM concert_schedule e WHERE e.id = :id")
+    Optional<Object> findByIdWithLock(@Param("id") Long scheduleId);
 }
