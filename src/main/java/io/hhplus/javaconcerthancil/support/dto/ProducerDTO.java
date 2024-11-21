e205e34790979b9def7a83e41d5113baefc0c6ff
THIRSTY <beta1992@hanmail.net>
2024-11-21 10:02:31 +0900

    fix 역직렬화를 위한 기본 생성자 및 롬복 Setter 추가

--- src/main/java/io/hhplus/javaconcerthancil/support/dto/ProducerDTO.java
+++ src/main/java/io/hhplus/javaconcerthancil/support/dto/ProducerDTO.java
@@ -1,26 +1,15 @@
 package io.hhplus.javaconcerthancil.support.dto;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class ProducerDTO {
 
     private String topic;
     private String key;
     private String message;
 
-    public ProducerDTO(String topic, String key, String message) {
-        this.topic = topic;
-        this.key = key;
-        this.message = message;
-    }
-
-    public String getTopic() {
-        return topic;
-    }
-
-    public String getKey() {
-        return key;
-    }
-
-    public String getMessage() {
-        return message;
-    }
 }
