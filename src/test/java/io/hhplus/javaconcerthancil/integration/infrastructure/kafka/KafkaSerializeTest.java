2eec17f3e8279498f8f668c5d63afab82c2f373d
THIRSTY <beta1992@hanmail.net>
2024-11-21 10:42:09 +0900

    fix 직렬화/역직렬화 단위테스트

---
+++ src/test/java/io/hhplus/javaconcerthancil/integration/infrastructure/kafka/KafkaSerializeTest.java
@@ -0,0 +1,29 @@
+package io.hhplus.javaconcerthancil.integration.infrastructure.kafka;
+
+import io.hhplus.javaconcerthancil.support.dto.ProducerDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class KafkaSerializeTest {
+    byte[] serialized;
+
+    @BeforeEach
+    void setUp() {
+        ProducerDTO producerDTO = new ProducerDTO("TEST","KEY","MESSAGE");
+        JsonSerializer jsonSerializer = new JsonSerializer();
+        this.serialized = jsonSerializer.serialize("TEST", producerDTO);
+    }
+
+    @Test
+    void name() {
+        JsonDeserializer<ProducerDTO> deserializer = new JsonDeserializer<>(ProducerDTO.class, false);
+        ProducerDTO producerDTO = deserializer.deserialize("TEST", serialized);
+        assertThat(producerDTO.getMessage()).isEqualTo("MESSAGE");
+    }
+
+
+}
